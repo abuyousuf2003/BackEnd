c81eb83d87424103e83ed48654efcb418b11cf5a
abuyousuf2003 <abuyousufmdjumman@gmail.com>
2024-10-15 12:26:50 +0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/facebook',(req,res)=>{
+res.send('abuyousufmdjumman')
+})
+app.get('/youtube',(req,res)=>{
+    res.send("welcome to youtube")
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
